3f60dad0e8b18c6c871966ab5e33db65d55c7c31
AccessCoder <info.unaddict@gmail.com>
2025-04-14 16:59:29 +0200

    add updated Code

--- src/main/java/ShopService.java
+++ src/main/java/ShopService.java
@@ -1,3 +1,4 @@
+import java.time.Instant;
 import java.util.ArrayList;
 import java.util.List;
 import java.util.UUID;
@@ -6,19 +7,32 @@ public class ShopService {
     private ProductRepo productRepo = new ProductRepo();
     private OrderRepo orderRepo = new OrderMapRepo();
 
-    public Order addOrder(List<String> productIds) {
+    public Order addOrder(List<String> productIds) throws ProductNotFoundException {
         List<Product> products = new ArrayList<>();
+
         for (String productId : productIds) {
-            Product productToOrder = productRepo.getProductById(productId);
-            if (productToOrder == null) {
-                System.out.println("Product mit der Id: " + productId + " konnte nicht bestellt werden!");
-                return null;
-            }
+            Product productToOrder = productRepo.getProductById(productId)
+                    .orElseThrow(() -> new ProductNotFoundException("Product with Id: "+ productId +" not found"));
+
             products.add(productToOrder);
         }
 
-        Order newOrder = new Order(UUID.randomUUID().toString(), products);
+        Order newOrder = new Order(UUID.randomUUID().toString(), products, OrderStatus.PROCESSING, Instant.now());
 
         return orderRepo.addOrder(newOrder);
+    }
+
+    public List<Order> getOrderByStatus(OrderStatus status) {
+        return orderRepo.getOrders().stream()
+                .filter(order -> order.status().equals(status))
+                .toList();
+    }
+
+    public OrderStatus updateStatus(String id, OrderStatus status) {
+        Order order = orderRepo.getOrderById(id)
+                .withStatus(status);
+        orderRepo.removeOrder(id);
+        orderRepo.addOrder(order);
+        return order.status();
     }
 }
